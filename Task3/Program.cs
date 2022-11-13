833c920cccfbcdeaee0a08e3b34c6b9ccb6798b0
VladBorod <jazgigantsmith@gmail.com>
2022-11-13 14:11:56 +0300

    Task3 works

---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 3.
+// Программа принимающая число и определяющая, является ли число четным.
+
+Console.Clear();
+
+// Ввод числового значения.
+
+Console.WriteLine("Even / odd number definition.");
+Console.Write("Enter the number: ");
+decimal number = decimal.Parse(Console.ReadLine());
+
+/*
+    Производим целочисленное деление,
+    с вычислением остатка.
+    В случае если остаток (%) = 0 - число четное (Even).
+    В случае если остаток (%) не равен 0 - не четное (Odd).
+*/
+
+if (number % 2 == 0)
+{
+    // YES!
+    Console.WriteLine($"Even number = {number}");
+}
+else 
+{
+    // NO!
+    Console.WriteLine($"Odd number = {number}");
+}
